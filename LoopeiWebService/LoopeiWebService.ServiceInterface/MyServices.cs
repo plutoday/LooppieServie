af5448570c1b10317505fb9851ae74564ad48d04
Xiaoxuan Li <xiaoxuan.li@bellevuecollege.edu>
2018-12-07 15:28:35 -0800

    Api and core

---
+++ LoopeiWebService/LoopeiWebService.ServiceInterface/MyServices.cs
@@ -0,0 +1,13 @@
+﻿using ServiceStack;
+using LoopeiWebService.ServiceModel;
+
+namespace LoopeiWebService.ServiceInterface
+{
+    public class MyServices : Service
+    {
+        public object Any(Hello request)
+        {
+            return new HelloResponse { Result = $"Hello, {request.Name}!" };
+        }
+    }
+}
